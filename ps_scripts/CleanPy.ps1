d8771dc47465f79c848d2255ecca6d106fa0923f
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2021-02-15 13:43:02 +0800

    Update CleanPy.ps1 for Python changes
    -Change for data\app folder
    -Retire config ini files deletion
    -Introduce .log files deletion

--- ps_scripts/CleanPy.ps1
+++ ps_scripts/CleanPy.ps1
@@ -5,16 +5,13 @@ Set-Location (Get-Location | Split-Path)
 if (Test-Path -Path "__pycache__") {
     Remove-Item "__pycache__" -Recurse
 }
-if (Test-Path -Path "app_data") {
-    Remove-Item "app_data" -Recurse
-}
-if (Test-Path -Path "data") {
-    Remove-Item "data" -Recurse
+if (Test-Path -Path "data\app") {
+    Remove-Item "data\app" -Recurse
 }
 
 # Check if file exist, delete if yes
-if (Test-Path "*.ini") {
-    Remove-Item "*.ini"
+if (Test-Path "*.log") {
+    Remove-Item "*.log"
 }
 
 Read-Host "Completed!`nPress any key to continue..."
