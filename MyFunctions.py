a762aab81a5f68a966cb110901d81d18d2631f4f
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2021-02-15 13:39:52 +0800

    Add new Python functions
    -is_internet_connected
    -parse_datetime
    -str_to_bool
    -wait_file_exist
    -del_file

--- MyFunctions.py
+++ MyFunctions.py
@@ -39,3 +39,37 @@ def handle_exception(log_message):
     logger.error(log_message, exc_info=True)
     logger.error("App run fail!" + LOG_END)
     exit()
+
+def is_internet_connected():
+    try:
+        urlopen("https://www.google.com.sg/", timeout=3)
+        return True
+    except Exception:
+        return False
+
+def parse_datetime(dt_input, input_format="", output_format=""):
+    dt = dt_input
+    if type(dt_input) == str:
+        dt = datetime.strptime(dt_input, input_format)
+    
+    if output_format != "":
+        return dt.strftime(output_format)
+    return dt
+
+def str_to_bool(bool_str):
+    if bool_str == "True":
+        return True
+    return False
+
+def wait_file_exist(file_path, my_timeout):
+    for i in range(my_timeout):
+        if os.path.isfile(file_path):
+            return True
+        sleep(1)
+    return False
+
+def del_file(file_path):
+    try:
+        os.remove(file_path)
+    except FileNotFoundError:
+        logger.error("File not found!")
